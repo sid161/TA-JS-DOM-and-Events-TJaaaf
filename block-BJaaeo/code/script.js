1cf550ac82ecaa0663c513cdd752a78dd49d5459
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-04-01 15:50:03 +0530

    Calculator

--- block-BJaaeo/code/script.js
+++ block-BJaaeo/code/script.js
@@ -1,33 +1,45 @@
-let display = document.querySelector(".display");
-let keys = document.querySelector(".keys");
-keys.addEventListener("click", handleClick);
+let display = document.querySelector('.display');
+let keys = document.querySelector('.keys');
 
-function handleClick(event) {
-    console.log(event.target.innerText);
-  
+keys.addEventListener('click', handleClick);
 
-    if(event.target.classList.contains('number')) {
-        if(display.innerText.endsWith("+") ||
-           display.innerText.endsWith("-") ||
-           display.innerText.endsWith("*") ||
-           display.innerText.endsWith("/") 
-           ) {
-             display.innerText = display.innerText +" " +event.target.dataset.content; 
-           } else {
-            display.innerText = display.innerText +event.target.dataset.content; 
-           }
-
-        console.log(display.innerText);
-    }
-    if (event.target.classList.contains('operation')) {        
-        display.innerText = display.innerText + event.target.dataset.content; 
-        console.log(display.innerText);        
-    }
-     if (event.target.dataset.content === 'C') {
-        display.innerText = "";
-      }  
-    if(event.target.dataset.content === '='){
-        display.innerText = eval(display.innerText);
-    }  
-
+function handleClick(event){
+  console.log(event.target.innerText);
 }
+
+if(event.target.classList.contains('number')){
+  if(display.innerText.endsWith("+") ||
+  display.innerText.endsWith("-") ||
+  display.innerText.endsWith("*") ||
+  display.innerText.endsWith("/")
+  ) {
+    display.innerText = display.innerText +" " +event.target.dataset.content;
+  } else {
+    display.innerText = display.innerText + event.target.dataset.content;
+  }
+
+  console.log(display.innerText);
+}
+
+if (event.target.classList.contains('operation')) {        
+  display.innerText = display.innerText + event.target.dataset.content; 
+  console.log(display.innerText);        
+}
+if (event.target.dataset.content === 'C') {
+  display.innerText = "";
+}  
+if(event.target.dataset.content === '='){
+  display.innerText = eval(display.innerText);
+}  
+
+
+
+
+
+
+
+
+
+
+
+
